9b20de3168f210a4fc27dc50dc9e60aca5521317
Gabriel Rodrigues <gbo10rodrigues@gmail.com>
2024-08-31 15:46:34 -0300

    template function

--- exe6/delivery_function/script.py
+++ exe6/delivery_function/script.py
@@ -1,3 +1,5 @@
+# Descomente a linha debaixo para testes locais, usar a função handler pra input da lambda
+# if __name__ == "__main__":
 def handler(event, _):
     try:
         print('Finished script')
