37e14d292b2051c5e04475cad3a14972a6ad303b
vsoldera <victorsolderalf@gmail.com>
2024-09-08 12:20:08 -0300

    adding soda sql MORE CHECKS

--- exe5/scriptsoda.py
+++ exe5/scriptsoda.py
@@ -1,22 +1,19 @@
 import subprocess
 
 def run_soda_scan():
-    # Define the Soda scan command
+
     command = ["soda", "scan", "-d", "adventureworks", "-c", "configuration.yml", "checks.yml"]
 
     try:
-        # Run the Soda scan command
         result = subprocess.run(command, capture_output=True, text=True)
 
-        # Check if the command was successful
         if result.returncode == 0:
             print("Soda scan completed successfully.")
-            print(result.stdout)  # Display the command output
+            print(result.stdout)  
         else:
             print("Soda scan failed.")
-            print(result.stderr)  # Display the error output
+            print(result.stderr)  
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# Execute the function
 run_soda_scan()
